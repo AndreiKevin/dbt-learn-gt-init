5e9448dde6bcfaa973beb3a16322a8a1f90ed4e1
AndreiKevin <81185469+AndreiKevin@users.noreply.github.com>
2025-10-22 00:57:56 +0800

    add: orders with sum of payments

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,61 @@
+with customers as (
+
+    select * from {{ ref('stg_jaffle_shop__customers')}}
+
+),
+
+orders as (
+    select * from {{ ref('stg_jaffle_shop__orders') }}
+),
+
+orders_payments as (
+    select * from {{ ref('fct_orders') }}
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders,
+        sum(orders_payments.amount) as lifetime_value
+
+    from orders
+    left join orders_payments using (order_id)
+
+    group by 1
+
+),
+
+payments as (
+    select * from {{ ref('stg_stripe__payments')}}
+),
+
+payments_agg as (
+    select orders.customer_id, sum(payments.amount) as lifetime_value
+    from orders
+    left join payments using (order_id)
+    where payments.status = 'success'
+    group by orders.customer_id
+),
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        payments_agg.lifetime_value
+
+    from customers
+
+    left join customer_orders using (customer_id)
+    left join payments_agg using (customer_id)
+)
+
+select * from final
